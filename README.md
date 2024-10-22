d48d9d8e90a2573dad0d4b95667bb3554109cf95
Popa Vlad <vladpopa1109@gmail.com>
2024-10-22 22:14:26 +0300

    Improved accuracy. Everything works as expected now.

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Emul8tor
 
-Emul8tor is an emulator for the CHIP-8 architecture.
+Emul8tor is an emulator for the CHIP-8 architecture, written in only 400 lines of code.
+
+The goal of this project is to teach myself (and others, perhaps, but beware of the lack of comments and messy code, since another one of my goals for this emulator was to attempt to write it in as few lines of code as possible without cheating too much.) about the basics of emulation.
+
+## Features
+
+Besides the beeper (which will probably never be added), the emulator is pretty accurate. I've tested it with about 15 ROMs, including an 8 ROM test suite, and they all worked fine.
+
+## Building and Running
+
+### Linux
+In the root directory, run the following commands:
+```
+mkdir build
+cd build
+cmake ..
+make
+./emul8tor path/to/your/rom.ch8
+```
+
+### Windows
+I dunno... I haven't tested it, but the process should be similar to how you'd do it on Linux. If you have WSL then you can simply build and run it there.
--- chip8.c
+++ chip8.c
@@ -1,5 +1,4 @@
-#include <SDL2/SDL_audio.h>
-#include <SDL2/SDL_events.h>
+#include <bits/time.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -10,14 +9,15 @@
 #include <fcntl.h>
 #include <SDL2/SDL.h>
 
+#define CPU_CLOCK_HZ 540
+#define TIMER_HZ 60
+#define CYCLES_PER_TIMER_TICK (CPU_CLOCK_HZ / TIMER_HZ)
+#define TIME_BETWEEN_TICKS (1000000 / TIMER_HZ)
+
 typedef struct {
 	uint8_t V[16];
-	uint16_t I;
-	uint8_t SP;
-	uint16_t PC;
-
-	uint8_t DT;
-	uint8_t ST;
+	uint16_t I, SP, PC;
+	uint8_t DT, ST;
 
 	uint16_t stack[16];
 	uint8_t displayFB[64*32];
@@ -49,10 +49,8 @@ SDL_Window* window;
 SDL_Renderer* renderer;
 
 int chip8_prepare_memory(chip8_t* chip8, const char* rompath){
-	// Load Font
 	memcpy((void*)chip8->memory, (void*)buildinFont, 80);
 
-	// Load ROM
 	int rom = open(rompath, 0);
 	if(rom < 0){
 		fprintf(stderr, "Cound not find specified ROM.\n");
@@ -80,13 +78,10 @@ void chip8_display_update(chip8_t* chip8){
 	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
 	SDL_Rect rect;
-
 	for(int i = 0; i < 32; i++){
 		for(int j = 0; j < 64; j++){
 			if(chip8->displayFB[i*64 + j]){
-				rect.x = j*8;
-				rect.y = i*8;
-				rect.h = rect.w = 8;
+				rect.x = j*8; rect.y = i*8; rect.h = rect.w = 8;
 				SDL_RenderFillRect(renderer, &rect);
 			}
 		}
@@ -101,67 +96,42 @@ int chip8_execute(chip8_t* chip8){
 	uint8_t nibble3 = (chip8->memory[chip8->PC+1] & 0xF0) >> 4;
 	uint8_t nibble4 = (chip8->memory[chip8->PC+1] & 0x0F);
 
-	static double totalSeconds = 0;
-	double secondsStart = clock();
-	double secondsStop;
-
 	switch(nibble1){
 		case 0:
-		{
 			if(nibble3 == 0xE && nibble4 == 0xE){
 				if(chip8->SP == 0) return -1;
 				chip8->SP--;
 				chip8->PC = chip8->stack[chip8->SP] + 2;
-				chip8->PC -= 2;
+				return 0;
 			} else if(nibble3 == 0xE){
-				for(int i = 0; i < 32*64; i++){
-					chip8->displayFB[i] = 0;
-				}	
+				for(int i = 0; i < 32*64; i++) chip8->displayFB[i] = 0;
 			}
 			break;
-		}
 		case 1:
-		{
 			chip8->PC = ((nibble2 << 8) | (nibble3 << 4) | (nibble4));
-			chip8->PC -= 2;
-			break;
-		}
+			return 0;
 		case 2:
-		{
 			chip8->stack[chip8->SP] = chip8->PC;
 			chip8->PC = ((nibble2 << 8) | (nibble3 << 4) | (nibble4));
 			chip8->SP++;
-			chip8->PC -= 2;
-			break;
-		}
+			return 0;
 		case 3:
-		{
 			if(chip8->V[nibble2] == ((nibble3 << 4) | (nibble4))) chip8->PC += 2;
 			break;
-		}
 		case 4:
-		{
 			if(chip8->V[nibble2] != ((nibble3 << 4) | (nibble4))) chip8->PC += 2;
 			break;
-		}
 		case 5:
-		{
 			if(chip8->V[nibble2] == chip8->V[nibble3]) chip8->PC += 2;
 			break;
-		}
 		case 6:
-		{
 			chip8->V[nibble2] = ((nibble3 << 4) | (nibble4));
 			break;
-		}
 		case 7:
-		{
 			if(chip8->V[nibble2] + ((nibble3 << 4) | (nibble4)) > 256) chip8->V[nibble2] = ((nibble3 << 4) | (nibble4)) - (255-chip8->V[nibble2]) - 1;
 			else chip8->V[nibble2] = chip8->V[nibble2] + ((nibble3 << 4) | (nibble4));
 			break;
-		}
 		case 8:
-		{
 			if(nibble4 == 0){
 				chip8->V[nibble2] = chip8->V[nibble3];
 			} else if(nibble4 == 1){
@@ -190,8 +160,8 @@ int chip8_execute(chip8_t* chip8){
 					chip8->V[0xF] = 0;
 				}
 			} else if(nibble4 == 6){
-				uint8_t lsb = chip8->V[nibble2] & 1;
-				chip8->V[nibble2] /= 2;
+				uint8_t lsb = chip8->V[nibble3] & 1;
+				chip8->V[nibble2] = chip8->V[nibble3]/2;
 				chip8->V[0xF] = lsb;
 			} else if(nibble4 == 7){
 				if(chip8->V[nibble3] >= chip8->V[nibble2]){
@@ -202,40 +172,29 @@ int chip8_execute(chip8_t* chip8){
 					chip8->V[0xF] = 0;
 				}	
 			} else if(nibble4 == 0xE){
-				uint8_t msb = ((chip8->V[nibble2] >> 7) & 1);
-				chip8->V[nibble2] *= 2;
+				uint8_t msb = ((chip8->V[nibble3] >> 7) & 1);
+				chip8->V[nibble2] = chip8->V[nibble3] * 2;
 				chip8->V[0xF] = msb;
 			} else {
 				return -1;	
 			}
 			break;
-		}
 		case 9:
-		{
 			if(chip8->V[nibble2] != chip8->V[nibble3]) chip8->PC += 2;
 			break;
-		}
 		case 0xA:
-		{
 			chip8->I = ((nibble2 << 8) | (nibble3 << 4) | (nibble4));
 			break;
-		}
 		case 0xB:
-		{
 			chip8->PC = ((nibble2 << 8) | (nibble3 << 4) | (nibble4)) + chip8->V[0];
-			chip8->PC -= 2;
-			break;
-		}
+			return 0;
 		case 0xC:
-		{
 			srand(time(NULL));
 			uint8_t random = rand() % 256;
 
 			chip8->V[nibble2] = (random & ((nibble3 << 4) | (nibble4)));
 			break;
-		}
 		case 0xD:
-		{
 			size_t x = chip8->V[nibble2];
 			size_t y = chip8->V[nibble3];
 
@@ -244,18 +203,15 @@ int chip8_execute(chip8_t* chip8){
 			for(int i = 0; i < nibble4; i++){
 				uint8_t spriteRow = chip8->memory[chip8->I+i];
 				for(int j = 0; j < 8; j++){
-						int dispIndex = ((x + j)%64) + (((y + i)%32)*64);
-						uint8_t currSprPixelState = ((spriteRow & (0b10000000 >> j))  >> (7-j));
-						if(chip8->displayFB[dispIndex] && currSprPixelState){
-							chip8->V[0xF] = 1;
-						}
-						chip8->displayFB[dispIndex] ^= currSprPixelState;
+					int dispIndex = ((x + j)%64) + (((y + i)%32)*64);
+					uint8_t currSprPixelState = ((spriteRow & (0b10000000 >> j))  >> (7-j));
+
+					if(chip8->displayFB[dispIndex] && currSprPixelState) chip8->V[0xF] = 1;
+					chip8->displayFB[dispIndex] ^= currSprPixelState;
 				}
 			}
 			break;
-		}
 		case 0xE:
-		{
 			if(nibble3 == 9 && nibble4 == 0xE){
 				if(chip8->V[nibble2] > 0xF) return -1;
 				if(keyboardState[chip8->V[nibble2]]) chip8->PC += 2;
@@ -264,29 +220,34 @@ int chip8_execute(chip8_t* chip8){
 				if(!keyboardState[chip8->V[nibble2]]) chip8->PC += 2;
 			}
 			break;
-		}
 		case 0xF:
-		{
 			if(nibble3 == 0 && nibble4 == 7){
 				chip8->V[nibble2] = chip8->DT;
 			} else if(nibble3 == 0 && nibble4 == 0xA){
-				for(int i = 0; i < 16; i++){
-					if(keyboardState[i]){
-						chip8->V[nibble2] = i;
-						break;
+				static int key = 0;
+				static bool keyPressDetected = false;
+
+				if(!keyPressDetected){
+					for(int i = 0; i < 16; i++){
+						if(keyboardState[i]){
+							keyPressDetected = true;
+							key = i;
+							break;
+						}
 					}
+				} else {
+					if(!keyboardState[key]){
+						keyPressDetected = false;
+						chip8->V[nibble2] = key;
+						chip8->PC += 2;
+					}	
 				}
-				chip8->PC -= 2;
-			} else if(nibble3 == 1 && nibble4 == 0x5){
-				chip8->DT = chip8->V[nibble2];
-			} else if(nibble3 == 1 && nibble4 == 8){
-				chip8->ST = chip8->V[nibble2];
-			} else if(nibble3 == 1 && nibble4 == 0xE){
-				chip8->I += chip8->V[nibble2];
-			} else if(nibble3 == 2 && nibble4 == 9){
-				if(chip8->V[nibble2] > 0xF) return -1;
-				chip8->I = 5*chip8->V[nibble2];
-			} else if(nibble3 == 3 && nibble4 == 3){
+				return 0;
+			} else if(nibble3 == 1 && nibble4 == 0x5) chip8->DT = chip8->V[nibble2];
+			else if(nibble3 == 1 && nibble4 == 8) chip8->ST = chip8->V[nibble2];
+			else if(nibble3 == 1 && nibble4 == 0xE) chip8->I += chip8->V[nibble2];
+			else if(nibble3 == 2 && nibble4 == 9) chip8->I = 5*(chip8->V[nibble2] & 0xF);
+			else if(nibble3 == 3 && nibble4 == 3){
 				chip8->memory[chip8->I] = chip8->V[nibble2]/100%10;
 				chip8->memory[chip8->I+1] = chip8->V[nibble2]/10%10;
 				chip8->memory[chip8->I+2] = chip8->V[nibble2]%10;
@@ -300,26 +261,8 @@ int chip8_execute(chip8_t* chip8){
 				return -1;
 			}
 			break;
-		}
 		default:
 			return -1;
-	}
-
-	secondsStop = clock();
-
-	totalSeconds += (secondsStop - secondsStart) / CLOCKS_PER_SEC;
-
-	if(totalSeconds >= 0.0016){
-		totalSeconds = 0;		
-
-		// Subtract 1 from DT/ST
-		if(chip8->DT > 0) chip8->DT--;
-
-		//TODO: sound.
-
-		if(chip8->ST > 0) chip8->ST--;
-
-		chip8_display_update(chip8);
 	}
 
 	chip8->PC += 2;
@@ -371,147 +314,83 @@ int main(int argc, char** argv){
 		return 1;
 	}
 
-
 	chip8_t* chip8 = chip8_init(argc, argv);
 	if(chip8 == NULL) return 1;
+
+	struct timespec startTime, endTime;
+	size_t elapsedTime, sleepTime;
 
 	SDL_Event event;
 	bool quit = false;
 	while(!quit){
-		while(SDL_PollEvent(&event)){
-			if(event.type == SDL_QUIT){
-				quit = true;
+		clock_gettime(CLOCK_MONOTONIC, &startTime);
+
+		for(size_t i = 0; i < CYCLES_PER_TIMER_TICK; i++){
+			while(SDL_PollEvent(&event)){
+				if(event.type == SDL_QUIT) quit = true;
+				else if(event.type == SDL_KEYDOWN){
+					SDL_KeyboardEvent kbEvent = event.key;
+					switch(kbEvent.keysym.sym){
+						case SDLK_0: keyboardState[0] = 1; break;
+						case SDLK_1: keyboardState[1] = 1; break;
+						case SDLK_2: keyboardState[2] = 1; break;
+						case SDLK_3: keyboardState[3] = 1; break;
+						case SDLK_4: keyboardState[4] = 1; break;
+						case SDLK_5: keyboardState[5] = 1; break;
+						case SDLK_6: keyboardState[6] = 1; break;
+						case SDLK_7: keyboardState[7] = 1; break;
+						case SDLK_8: keyboardState[8] = 1; break;
+						case SDLK_9: keyboardState[9] = 1; break;
+						case SDLK_a: keyboardState[10] = 1; break;
+						case SDLK_b: keyboardState[11] = 1; break;
+						case SDLK_c: keyboardState[12] = 1; break;
+						case SDLK_d: keyboardState[13] = 1; break;
+						case SDLK_e: keyboardState[14] = 1; break;
+						case SDLK_f: keyboardState[15] = 1; break;
+						default: break;
+					}
+				} else if(event.type == SDL_KEYUP){
+					SDL_KeyboardEvent kbEvent = event.key;
+					switch(kbEvent.keysym.sym){
+						case SDLK_0: keyboardState[0] = 0; break;
+						case SDLK_1: keyboardState[1] = 0; break;
+						case SDLK_2: keyboardState[2] = 0; break;
+						case SDLK_3: keyboardState[3] = 0; break;
+						case SDLK_4: keyboardState[4] = 0; break;
+						case SDLK_5: keyboardState[5] = 0; break;
+						case SDLK_6: keyboardState[6] = 0; break;
+						case SDLK_7: keyboardState[7] = 0; break;
+						case SDLK_8: keyboardState[8] = 0; break;
+						case SDLK_9: keyboardState[9] = 0; break;
+						case SDLK_a: keyboardState[10] = 0; break;
+						case SDLK_b: keyboardState[11] = 0; break;
+						case SDLK_c: keyboardState[12] = 0; break;
+						case SDLK_d: keyboardState[13] = 0; break;
+						case SDLK_e: keyboardState[14] = 0; break;
+						case SDLK_f: keyboardState[15] = 0; break;
+						default: break;
+
+					}
+				}
 			}
 
-			if(event.type == SDL_KEYDOWN){
-				SDL_KeyboardEvent kbEvent = event.key;
-				switch(kbEvent.keysym.sym){
-					case SDLK_0:
-						keyboardState[0] = 1;
-						break;
-					case SDLK_1:
-						keyboardState[1] = 1;
-						break;
-					case SDLK_2:
-						keyboardState[2] = 1;
-						break;
-					case SDLK_3:
-						keyboardState[3] = 1;
-						break;
-					case SDLK_4:
-						keyboardState[4] = 1;
-						break;
-					case SDLK_5:
-						keyboardState[5] = 1;
-						break;
-					case SDLK_6:
-						keyboardState[6] = 1;
-						break;
-					case SDLK_7:
-						keyboardState[7] = 1;
-						break;
-					case SDLK_8:
-						keyboardState[8] = 1;
-						break;
-					case SDLK_9:
-						keyboardState[9] = 1;
-						break;
-					case SDLK_a:
-						keyboardState[10] = 1;
-						break;
-					case SDLK_b:
-						keyboardState[11] = 1;
-						break;
-					case SDLK_c:
-						keyboardState[12] = 1;
-						break;
-					case SDLK_d:
-						keyboardState[13] = 1;
-						break;
-					case SDLK_e:
-						keyboardState[14] = 1;
-						break;
-					case SDLK_f:
-						keyboardState[15] = 1;
-						break;
-					default:
-						break;
-				}
-			} else if(event.type == SDL_KEYUP){
-				SDL_KeyboardEvent kbEvent = event.key;
-				switch(kbEvent.keysym.sym){
-					case SDLK_0:
-						keyboardState[0] = 0;
-						break;
-					case SDLK_1:
-						keyboardState[1] = 0;
-						break;
-					case SDLK_2:
-						keyboardState[2] = 0;
-						break;
-					case SDLK_3:
-						keyboardState[3] = 0;
-						break;
-					case SDLK_4:
-						keyboardState[4] = 0;
-						break;
-					case SDLK_5:
-						keyboardState[5] = 0;
-						break;
-					case SDLK_6:
-						keyboardState[6] = 0;
-						break;
-					case SDLK_7:
-						keyboardState[7] = 0;
-						break;
-					case SDLK_8:
-						keyboardState[8] = 0;
-						break;
-					case SDLK_9:
-						keyboardState[9] = 0;
-						break;
-					case SDLK_a:
-						keyboardState[10] = 0;
-						break;
-					case SDLK_b:
-						keyboardState[11] = 0;
-						break;
-					case SDLK_c:
-						keyboardState[12] = 0;
-						break;
-					case SDLK_d:
-						keyboardState[13] = 0;
-						break;
-					case SDLK_e:
-						keyboardState[14] = 0;
-						break;
-					case SDLK_f:
-						keyboardState[15] = 0;
-						break;
-					default:
-						break;
-				}
+			if(chip8_execute(chip8) != 0){
+				printf("ERROR: Failed to execute instruction at 0x%x\n\n", chip8->PC);
+				quit = true;
+				break;
 			}
 		}
 
-		if(chip8_execute(chip8) != 0){
-			printf("ERROR: Failed to execute instruction at 0x%x\n\n", chip8->PC);
+		clock_gettime(CLOCK_MONOTONIC, &endTime);
 
-			// Dump memory and stack
-			
-			printf("Memory dump at PC:\n");
-			for(int i = 0; i < 0x100; i++){	
-				printf("0x%x ", chip8->memory[0x200 + i]);
-				if(i%16==15) printf("\n");
-			}
+		elapsedTime = (endTime.tv_sec - startTime.tv_sec) * 1000000 + (endTime.tv_nsec - startTime.tv_nsec) / 1000;
+		sleepTime = TIME_BETWEEN_TICKS - elapsedTime;
 
-			printf("\n\nStack dump:\n");
-			for(int i = 0; i < 16; i++){
-				printf("0x%x ", chip8->stack[i]);
-			}
+		if(sleepTime > 0) usleep(sleepTime);
 
-			quit = true;
-		}	
+		if(chip8->DT > 0) chip8->DT--;
+		if(chip8->ST > 0) chip8->ST--;
+		chip8_display_update(chip8);	
 	}
 	
 	free(chip8);
